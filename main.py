512fffa78b1db81c7d6bfa992b186780cff35e45
Jainam-IITK <jainamsb1@gmail.com>
2024-03-12 07:00:45 +0530

    Remove extra stuff

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
-from stock_search import search
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/stock_search/{query}")
-def read_item(query: str):
-    return search(query)
 
 
 if __name__ == "__main__":
